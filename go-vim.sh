e73ae0b7c87be5bd4e647fbae3a228cec9147f67
Mike Dulaney <k.michael.dulaney@gmail.com>
2013-12-02 11:31:51 -0500

    Added vim golang highlighting support for ubuntu linux

--- go-vim.sh
+++ go-vim.sh
@@ -6,4 +6,9 @@ mkdir -p $HOME/.vim/autoload/go
 ln -s $GOROOT/misc/vim/ftdetect/gofiletype.vim $HOME/.vim/ftdetect/
 ln -s $GOROOT/misc/vim/syntax/go.vim $HOME/.vim/syntax
 ln -s $GOROOT/misc/vim/autoload/go/complete.vim $HOME/.vim/autoload/go
+
+echo "filetype off" >> $HOME/.vimrc
+echo "filetype plugin indent off" >> $HOME/.vimrc
+echo "set runtimepath+=$GOROOT/misc/vim" >> $HOME/.vimrc
+echo "filetype plugin indent on" >> $HOME/.vimrc
 echo "syntax on" >> $HOME/.vimrc
