95582b2fd40d7b384cbc2aa7a464ed87ca7c34b2
Mike Dulaney <k.michael.dulaney@gmail.com>
2013-12-07 17:48:16 -0500

    Basic git aliases

--- git-config.sh
+++ git-config.sh
@@ -4,3 +4,10 @@ git config --global user.name "Mike Dulaney"
 git config --global user.email k.michael.dulaney@gmail.com
 git config --global color.ui auto
 
+echo "[alias]
+  st = status
+  ci = commit
+  br = branch
+  co = checkout
+  df = diff
+  lg = log -p" >> ~/.gitconfig
