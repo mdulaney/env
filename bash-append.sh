87cea1f3825866da4df95dbf9c90f37c2d4ee046
Mike Dulaney <k.michael.dulaney@gmail.com>
2015-05-17 19:17:47 -0400

    One script to rule them all

--- bash-append.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "set -o vi" >> ~/.bashrc
-echo "export EDITOR=vim" >> ~/.bashrc
-echo export PS1=\"$ \" >> ~/.bashrc
--- setup-apt-pkgs.sh
+++
@@ -1,1 +0,0 @@
-sudo apt-get install vim wireshark tmux xmonad gcc gdb ctags cifs-utils chromium-browser irssi python-dev python-setuptools python-pip
